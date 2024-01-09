cae09c06e7e2291299022cd45c00b6ad1c19840e
speed1313 <speedtry13@icloud.com>
2024-01-09 19:25:27 +0900

    fix

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Neural Tangent Kernel Experiments
 
+Experimental code to investigate the validity of Neural Tanget Kernel (NTK) theory in finite width.
+The blog post about this experiment is [here](https://speed1313.github.io/posts/ntk/).
 
 
 # How to Use
 ```
-$ rye run python3 src/ntk/ntk_experiment.py
+$ rye run python3 src/ntk/ntk_expermient.py --epoch 20000 --mode minibatch --batch_size 5 --depth 5 --lr 0.001
 ```
+
--- src/ntk/ntk_expermient.py
+++ src/ntk/ntk_expermient.py
@@ -11,39 +11,19 @@ import csv
 
 class MLP(nn.Module):
     width: int
+    depth: int
 
     def setup(self):
-        self.dense1 = nn.Dense(features=self.width)
-        self.dense2 = nn.Dense(features=self.width)
-        self.dense3 = nn.Dense(features=self.width)
-        self.dense4 = nn.Dense(features=1)
+        for i in range(self.depth):
+            setattr(self, f"dense{i}", nn.Dense(features=self.width))
+        self.dense = nn.Dense(features=1)
 
     def __call__(self, x):
-        x = self.dense1(x)
-        x = jax.nn.relu(x)
-        x = self.dense2(x)
-        x = jax.nn.relu(x)
-        x = self.dense3(x)
-        x = jax.nn.relu(x)
-        x = self.dense4(x)
+        for i in range(self.depth):
+            x = getattr(self, f"dense{i}")(x)
+            x = jax.nn.relu(x)
+        x = self.dense(x)
         return x
-
-
-"""
-def NTK(model, params, x, y):
-    def f(params, x):
-        return model.apply(params, x)
-
-    J_x = jax.jacrev(f, argnums=0)(params, x)
-    # J_y = jax.jacfwd(f, argnums=0)(params, y)
-    grad_x = jax.flatten_util.ravel_pytree(J_x)[0]
-    # del J_x
-    # grad_y = jax.flatten_util.ravel_pytree(J_y)[0]
-    # del J_y
-    ntk = grad_x @ grad_x.T
-    # del grad_x, grad_y
-    return ntk
-"""
 
 
 def split_into_batches_random(arr, batch_size, rng_key):
@@ -59,65 +39,80 @@ def split_into_batches_random(arr, batch_size, rng_key):
     if num_batches * batch_size < arr.shape[0]:
         remaining_batch = arr_shuffled[num_batches * batch_size :]
         batches.append(remaining_batch)
-    return batches
+    return batches, rng_key
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--width", type=int, default=1000)
 parser.add_argument("--epoch", type=int, default=1000)
 parser.add_argument("--lr", type=float, default=0.001)
-parser.add_argument("--mode", type=str, default="sgd")
+parser.add_argument("--mode", type=str, default="minibatch")
 parser.add_argument("--batch_size", type=int, default=2)
+parser.add_argument("--depth", type=int, default=3)
 args = parser.parse_args()
 
 mode = args.mode
 batch_size = args.batch_size
-
 lr = args.lr
-
+widths = [1000, 100, 10]  # , 2000]
+num_width = len(widths)
 
 now_time = datetime.datetime.now()
 dir = "images/" + now_time.strftime("%Y/%m/%d/%H%M%S")
 os.makedirs(dir, exist_ok=True)
 
 # output config file
+# first row keys
+# second row values
 with open(f"{dir}/config.csv", "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["lr", "mode", "epoch", "width"])
-    writer.writerow([lr, mode, args.epoch, args.width])
+    first_row = []
+    for key in vars(args).keys():
+        first_row.append(key)
+    writer.writerow(first_row)
+    second_row = []
+    for value in vars(args).values():
+        second_row.append(value)
+    writer.writerow(second_row)
 
 
 loss_dynamics_list = []
-
-widths = [10, 100, 1000]
-num_width = len(widths)
-
 norm_list = []
 loss_list = []
-param_list = []
-train_num = 10
-x_train = jnp.linspace(-3, 3, train_num)
-x_train = jnp.expand_dims(x_train, axis=-1)
-
+ntk_list_per_width = [] * num_width
 
 def true_fn(x):
-    # return (x ** 2) / 2 + x + 1
-    return jnp.sin(x)
+    return x**3 / 20
+    # return jnp.sin(x)
 
-
+# train data
+train_num = 10
+x_train = jnp.linspace(-5, 5, train_num)
+x_train = jnp.expand_dims(x_train, axis=-1)
 y_train = jax.vmap(true_fn)(x_train).reshape(-1)
 
-x = jnp.linspace(-5, 5, 100)
+# plot data
+x = jnp.linspace(-10, 10, 100)
 x = jnp.expand_dims(x, axis=-1)
 y = []
-ntk_list_per_width = [] * num_width
+
+
+plt.scatter(x_train, y_train, label="train data")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Train data")
+plt.legend()
+plt.savefig(f"{dir}/train_data.png")
+plt.show()
+
+
+
 
 for width in widths:
     params_norm_list = []
     loss_dynamics = []
-    key = jax.random.PRNGKey(0)
     ntk_list = []
-    model = MLP(width=width)
+    key = jax.random.PRNGKey(0)
+    model = MLP(width=width, depth=args.depth)
     params = model.init(key, x_train[0])
     init_params = params
 
@@ -130,15 +125,24 @@ for width in widths:
     # model's input shape is defined after mode.init
     @jax.jit
     def NTK(params, x, y):
-        J_x_fn = jax.jacrev(model.apply, argnums=0)
-        # J_y = jax.jacfwd(f, argnums=0)(params, y)
-        J_x = J_x_fn(params, x)
+        """
+        J_fn = jax.jacrev(model.apply, argnums=0)
+        J_x = J_fn(params, x)
         grad_x = jax.flatten_util.ravel_pytree(J_x)[0]
-        # del J_x
-        # grad_y = jax.flatten_util.ravel_pytree(J_y)[0]
-        # del J_y
+        J_y = J_fn(params, y)
+        grad_y = jax.flatten_util.ravel_pytree(J_y)[0]
+        ntk = grad_x @ grad_y.T
+        return ntk
+        """
+
+        def f(model, params, x):
+            return model.apply(params, x)[0]
+
+        grad_x = jax.grad(f, argnums=1)(model, params, x)
+        grad_x = jax.flatten_util.ravel_pytree(grad_x)[0]
+        # grad_y = jax.grad(f, argnums=1)(model, params, y)
+        # grad_y = jax.flatten_util.ravel_pytree(grad_y)[0]
         ntk = grad_x @ grad_x.T
-        # del grad_x, grad_y
         return ntk
 
     ntk_init = NTK(params, x_train[0], x_train[1])
@@ -161,7 +165,7 @@ for width in widths:
                 params = jax.tree_util.tree_map(lambda p, g: p - lr * g, params, grad)
         elif mode == "minibatch":
             idxs = jnp.arange(len(x_train))
-            batches = split_into_batches_random(idxs, batch_size, key)
+            batches, key = split_into_batches_random(idxs, batch_size, key)
 
             for batch in batches:
                 batch_x = x_train[batch]
@@ -191,36 +195,38 @@ for width in widths:
     loss_dynamics_list.append(loss_dynamics)
     norm_list.append(params_norm_list)
     loss_list.append(loss_dynamics)
-    param_list.append(params)
     y.append(jax.vmap(model.apply, in_axes=(None, 0))(params, x))
 
     ntk_list_per_width.append(ntk_list)
 
 epoch_list = [i * 5 for i in range(args.epoch // 5)]
 for i in range(num_width):
-    plt.plot(epoch_list, loss_list[i], label=f"width={10 ** (i + 1)}")
+    plt.plot(epoch_list, loss_list[i], label=f"width={widths[i]}")
 plt.xlabel("epoch(t)")
 plt.ylabel("loss")
-plt.ylim(0, 1)
+plt.ylim(0, 5)
+plt.xscale("log")
 plt.title("Training loss")
 plt.legend()
 plt.savefig(f"{dir}/loss_epoch_{args.epoch}.png")
 plt.show()
 for i in range(num_width):
-    plt.plot(epoch_list, norm_list[i], label=f"width={10 ** (i + 1)}")
+    plt.plot(epoch_list, norm_list[i], label=f"width={widths[i]}")
 
 plt.xlabel("epoch(t)")
 plt.ylabel("$\\frac{\|w(t) - w(0)\|}{\|w(0)\|}$")
 plt.title("Parameter change")
 plt.legend()
+plt.xscale("log")
 plt.savefig(f"{dir}/param_norm_epoch_{args.epoch}.png")
 plt.show()
 
 
 plt.scatter(x_train, y_train, label="train data")
 for i in range(num_width):
-    plt.plot(x, y[i], label=f"width={10 ** (i + 1)}")
-plt.ylim(-5, 5)
+    plt.plot(x, y[i], label=f"width={widths[i]}")
+
+plt.xlim(-10, 10)
 plt.xlabel("x")
 plt.ylabel("y")
 plt.title("Function approximation")
@@ -230,10 +236,11 @@ plt.show()
 
 epoch_list = [i * 5 for i in range(args.epoch // 5)]
 for i in range(num_width):
-    plt.plot(epoch_list, ntk_list_per_width[i], label=f"width={10 ** (i + 1)}")
+    plt.plot(epoch_list, ntk_list_per_width[i], label=f"width={widths[i]}")
 plt.xlabel("epoch(t)")
 plt.ylabel("$\\frac{\|NTK(t) - NTK(0)\|}{\|NTK(0)\|}$")
-plt.title("$NTK(x_{train[0]}, x_{train[1]})$")
+plt.title("$NTK(x_{train[0]}, x_{train[0]})$")
 plt.legend()
+plt.xscale("log")
 plt.savefig(f"{dir}/ntk_epoch_{args.epoch}.png")
 plt.show()
